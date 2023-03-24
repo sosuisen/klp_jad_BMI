3216b2f06ae5a136fef14832eac3ce0fab879f60
sosuisen <hidekazu.kubota@gmail.com>
2023-03-24 12:24:00 +0900

    feat: add answer

--- src/main/java/com/example/MyAppController.java
+++ src/main/java/com/example/MyAppController.java
@@ -1,28 +1,40 @@
 package com.example;
 
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MyAppController {
 
 	@FXML
-	private Button button;
+	private Button calcButton;
 
 	@FXML
-	private Label label;
+	private TextField heightField;
 
 	@FXML
-	void buttonOnAction(ActionEvent event) {
-		Platform.exit();
-	}
+	private Label resultLabel;
+
+	@FXML
+	private TextField weightField;
 
 	public void initialize() {
-		var javaVersion = System.getProperty("java.version");
-		var javafxVersion = System.getProperty("javafx.version");
-
-		label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+		calcButton.setOnAction(e -> {
+			double height = Double.parseDouble(heightField.getText());
+			double weight = Double.parseDouble(weightField.getText());
+			double bmi = 10000 * weight / Math.pow(height, 2);
+			// 小数点以下１桁まで。10でなく10.0で割ること。10で割ると整数になるため。
+			double bmiShort = Math.round(bmi * 10)/10.0;
+			String bmiStr = String.valueOf(bmiShort);
+			
+			// 判定
+			String judge = "";
+			if (bmi < 18.5) judge = "痩せ";
+			else if (bmi < 25) judge = "標準体重";
+			else judge = "肥満";
+			
+			resultLabel.setText("BMI = " + bmiStr + "【" + judge + "】");
+		});
 	}
 }
